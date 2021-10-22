514772f2b377e13da1b8ae1e90b78de9574fb162
m00n <71098497+0xm00n@users.noreply.github.com>
2021-10-22 17:29:13 -0400

    __main__

---
+++ Project/rest_api_server/__main__.py
@@ -0,0 +1,33 @@
+from tornado.web import Application, RequestHandler
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from tornado.platform.asyncio import AsyncIOMainLoop
+from handlers import *
+
+import motor 
+import os
+
+def main():
+    AsyncIOMainLoop().install()
+    ioloop = IOLoop.current()
+
+    application = Application([
+        (r"/api/v1", MainRequestHandler),
+        (r"/api/v1/order", order),
+        (r"/api/v1/marketData/realtime", realMarketData),
+        (r"/api/v1/marketData/historic", historicMarketData)
+    ],
+    debug=True,
+    autoreload=True)
+    
+    application.listen(1337)
+    print(f'server is listening on port 1337')
+
+    uri = os.environ["MONGO_URI"]
+    motor_client = motor.motor_tornado.MotorClient(uri)
+    application.settings['db'] = motor_client.orders
+
+    ioloop.start()
+
+if __name__ == '__main__':
+    main()
