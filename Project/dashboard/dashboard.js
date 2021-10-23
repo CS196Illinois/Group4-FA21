ee9270c5f840fa7911061ce61df5f9f2b8d202c5
djwyates <djwyates@gmail.com>
2021-10-23 15:19:51 -0500

    Added polling to update graph and table

--- Project/dashboard/dashboard.js
+++ Project/dashboard/dashboard.js
@@ -1,4 +1,15 @@
 let stockChart;
+let dataFromAPI;
+
+function renderGraphAndTable(firstTimeRun) {
+    // TODO: fetch data from API & set to dataFromAPI
+    if (firstTimeRun) {
+        initializeGraph();
+    } else {
+        refreshGraph();
+    }
+    renderTable();
+}
 
 function initializeGraph() {
     var data = [];
@@ -68,7 +79,7 @@ function refreshGraph() {
     stockChart.charts[0].set("data", data);
 }
 
-// TEMP - initial filler data //
+// *** TEMP - initial filler data //
 let dataPoints = [];
 let time = 1633053788;
 for (var i = 0; i < 15; i++) {
@@ -79,15 +90,53 @@ for (var i = 0; i < 15; i++) {
     time += 33000;
 }
 // *** //
+
 function getGraphDataPoints() {
-    // TODO: fetch & parse data from API and fill dataPoints
+    // TODO: populate graph with real data instead of filler data (loop through dataFromAPI and add to dataPoints as needed)
     //let dataPoints = [];
-    // TEMP - periodic filler data //
+
+    // *** TEMP - periodic filler data //
     dataPoints.push({
         "x": new Date(time * 1000),
         "y": Math.floor(Math.random() * 8000 + 92000)
     });
     time += 33000;
     // *** //
+
     return dataPoints;
+}
+
+const tableBody = document.querySelector("#tableBody");
+
+function renderTable() {
+    // TODO: populate table with real data instead of filler data (loop through dataFromAPI and add to HTML as needed)
+
+    // *** TEMP - periodic filler data
+    var pps = Math.round((Math.random() * 129 + 50) * 100) / 100;
+    var shares = Math.floor(Math.random() * 22 + 3);
+    var dt = new Date();
+
+    var newEntryHTML = `<tr>
+        <th>${ (dt.getMonth()+1).toString().padStart(2, '0')}-${
+            dt.getDate().toString().padStart(2, '0')}-${
+            dt.getFullYear().toString().padStart(4, '0')}, ${
+            dt.getHours().toString().padStart(2, '0')}:${
+            dt.getMinutes().toString().padStart(2, '0')}:${
+            dt.getSeconds().toString().padStart(2, '0') }
+        </th>
+        <td>${ (dt.getMonth()+1).toString().padStart(2, '0')}-${
+            dt.getDate().toString().padStart(2, '0')}-${
+            dt.getFullYear().toString().padStart(4, '0')}, ${
+            dt.getHours().toString().padStart(2, '0')}:${
+            dt.getMinutes().toString().padStart(2, '0')}:${
+            dt.getSeconds().toString().padStart(2, '0') }
+        </td>
+        <td><abbr title="Apple Inc.">AAPL</abbr></td>
+        <td>Market</td>
+        <td>$${pps}</td>
+        <td>${shares}</td>
+        <td class="has-text-success">+ $xx,xxx</td>
+    <tr>`;
+    tableBody.innerHTML = newEntryHTML + tableBody.innerHTML;
+    // *** //
 }
