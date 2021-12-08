ce990866328f182578cf777674de1385d14db70f
David Yates <djwyates@gmail.com>
2021-12-07 20:50:37 -0600

    populate trade log table with data from alpaca

--- Project/dashboard/dashboard.js
+++ Project/dashboard/dashboard.js
@@ -85,7 +85,7 @@ function refreshGraph() {
 
 function getGraphDataPoints() {
     let dataPoints = [];
-    for (var i = 0; i < portfolioInfo.equity.length; i++) {
+    for (let i = 0; i < portfolioInfo.equity.length; i++) {
         let date = new Date(0);
         date.setUTCSeconds(portfolioInfo.timestamp[i]);
         dataPoints.push({
@@ -99,34 +99,45 @@ function getGraphDataPoints() {
 const tableBody = document.querySelector("#tableBody");
 
 function renderTable() {
-    // TODO: populate table with real data instead of filler data (loop through portfolioInfo and add to HTML as needed)
-
-    // *** TEMP - periodic filler data
-    var pps = Math.round((Math.random() * 129 + 50) * 100) / 100;
-    var shares = Math.floor(Math.random() * 22 + 3);
-    var dt = new Date();
-
-    var newEntryHTML = `<tr>
-        <th>${ (dt.getMonth()+1).toString().padStart(2, '0')}-${
-            dt.getDate().toString().padStart(2, '0')}-${
-            dt.getFullYear().toString().padStart(4, '0')}, ${
-            dt.getHours().toString().padStart(2, '0')}:${
-            dt.getMinutes().toString().padStart(2, '0')}:${
-            dt.getSeconds().toString().padStart(2, '0') }
-        </th>
-        <td>${ (dt.getMonth()+1).toString().padStart(2, '0')}-${
-            dt.getDate().toString().padStart(2, '0')}-${
-            dt.getFullYear().toString().padStart(4, '0')}, ${
-            dt.getHours().toString().padStart(2, '0')}:${
-            dt.getMinutes().toString().padStart(2, '0')}:${
-            dt.getSeconds().toString().padStart(2, '0') }
-        </td>
-        <td><abbr title="Apple Inc.">AAPL</abbr></td>
-        <td>Market</td>
-        <td>$${pps}</td>
-        <td>${shares}</td>
-        <td class="has-text-success">+ $xx,xxx</td>
-    <tr>`;
-    tableBody.innerHTML = newEntryHTML + tableBody.innerHTML;
-    // *** //
+    let tableBodyHTML = "";
+    for (let i = 0; i < portfolioInfo.orders.length; i++) {
+        let order = portfolioInfo.orders[i];
+        let datePlaced = new Date(order.placed_at);
+        tableBodyHTML += `<tr><th>${ (datePlaced.getMonth()+1).toString().padStart(2, '0')}-${
+            datePlaced.getDate().toString().padStart(2, '0')}-${
+            datePlaced.getFullYear().toString().padStart(4, '0')}, ${
+            datePlaced.getHours().toString().padStart(2, '0')}:${
+            datePlaced.getMinutes().toString().padStart(2, '0')}:${
+            datePlaced.getSeconds().toString().padStart(2, '0') }
+        </th>`
+        if (order.filled_at !== "None") {
+            let dateExecuted = new Date(order.filled_at);
+            tableBodyHTML += `<td>${ (dateExecuted.getMonth()+1).toString().padStart(2, '0')}-${
+                dateExecuted.getDate().toString().padStart(2, '0')}-${
+                dateExecuted.getFullYear().toString().padStart(4, '0')}, ${
+                dateExecuted.getHours().toString().padStart(2, '0')}:${
+                dateExecuted.getMinutes().toString().padStart(2, '0')}:${
+                dateExecuted.getSeconds().toString().padStart(2, '0') }
+            </td>`;
+        } else {
+            tableBodyHTML += "<td>Unfilled</td>"
+        }
+        tableBodyHTML += `<td><abbr title="">${order.ticker}</abbr></td>`;
+        tableBodyHTML += `<td>${order.type.charAt(0).toUpperCase() + order.type.slice(1)}</td>`;
+        tableBodyHTML += `<td>${order.pps ? "$" + order.pps : "Unfilled"}</td>`;
+        if (order.filled_quantity === order.quantity || order.filled_quantity === "0") {
+            tableBodyHTML += `<td>${order.quantity}</td>`;
+        } else {
+            tableBodyHTML += `<td>${order.filled_quantity + " of " + order.quantity}</td>`;
+        }
+        if (order.filled_quantity === "0") {
+            tableBodyHTML += `<td class="has-text-link">Unfilled</td>`;
+        } else if (order.buy_or_sell === "buy") {
+            tableBodyHTML += `<td class="has-text-danger">- $${order.filled_quantity * order.filled_avg_price}</td>`;
+        } else {
+            tableBodyHTML += `<td class="has-text-success">+ $${order.filled_quantity * order.filled_avg_price}</td>`;
+        }
+        tableBodyHTML += "<tr>";
+    }
+    tableBody.innerHTML = tableBodyHTML;
 }
