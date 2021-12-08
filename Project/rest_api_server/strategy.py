78a7d4bd1eaa5b847859958971bc4f0e08fa678b
m00n <71098497+0xm00n@users.noreply.github.com>
2021-12-08 01:15:23 -0500

    fleshed out strategy class

--- Project/rest_api_server/strategy.py
+++ Project/rest_api_server/strategy.py
@@ -14,27 +14,27 @@ api = tradeapi.REST(
 
 class strategy():
     def __init__(self):
-        self.order_freq = 0
         self.rule_chain = []
+    
+    def __len__(self):
+        return len(self.rule_chain)
 
-    def addRule(order):
-        pass
+    def addRule(self, order):
+        self.rule_chain.append(order)
+        order.execute()
          
 
 
 class order():
-    def __init__(self):
-        pass 
+    def __init__(self, sym, qty):
+        self.symbol = sym
+        self.quantity = qty 
 
     def execute(self):
         pass
 
 
 class buy_order(order):
-    def __init__(self, sym, qty):
-        self.symbol = sym 
-        self.quantity = qty
-
     def execute(self):
         api.submit_order(
             symbol=self.symbol,
@@ -48,8 +48,7 @@ class buy_order(order):
 
 class limit_sell_order(order):
     def __init__(self, sym, qty, lp):
-        self.symbol = sym
-        self.quantity = qty 
+        super().__init__(sym, qty)
         self.lim_price = lp 
 
     def execute(self):
@@ -65,22 +64,85 @@ class limit_sell_order(order):
 
 
 class bracket_order(order):
-    def __init__(self, sym, qty, strat, perc=None):
-        self.symbol = sym
-        self.quantity = qty 
+    def __init__(self, sym, qty, strat, stop_perc, take_perc=None):
+        super().__init__(sym, qty)
         self.strategy = strat
+        self.stop_percentage = stop_perc
 
         if self.strategy == "both":
-            #percentage for stop-loss and take-profit
-            self.percentage = perc
+            self.take_percentage = take_perc
+
 
     def execute(self):
-        pass
+        symbol_bars = api.get_barset(self.symbol, "minute", 1).df.iloc[0]
+        symbol_price = symbol_bars[self.symbol]["close"]
+
+        if self.strategy == "both":
+            # buy a position and add a stop-loss and a take-profit of user specified percent
+            api.submit_order(
+                symbol=self.symbol,
+                qty=self.quantity,
+                side='buy',
+                type='market',
+                time_in_force='gtc',
+                order_class='bracket',
+                stop_loss={'stop_price': symbol_price * (1.0 - (self.stop_percentage / 100.0)),
+                           'limit_price':  symbol_price * (1.0 - (self.stop_percentage / 100.0) - 0.01)},
+                take_profit={'limit_price': symbol_price * (1.0 + (self.take_percentage / 100.0))}
+            )
+            print(Fore.GREEN + "[+] new bracket order (stop-loss + take-profit) submitted" + Fore.RESET + '\n')
+        
+        elif self.strategy == "stop-loss":
+            api.submit_order(
+                symbol=self.symbol,
+                qty=1,
+                side='buy',
+                type='market',
+                time_in_force='gtc',
+                order_class='oto',
+                stop_loss={'stop_price': symbol_price * (1.0 - (self.percent / 100.0))},
+            )
+            print(Fore.GREEN + "[+] new bracket order (stop-loss) submitted" + Fore.RESET + '\n')
+
 
 
 class trailing_stop_order(order):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, sym, qty, pp, price, percent):
+        super().__init__(sym, qty)
+        self.percent_or_price = pp
+
+        if self.percent_or_price == "price":
+            # a dollar value away from the highest water mark. 
+            # If you set this to 2.00 for a sell trailing stop, 
+            # the stop price is always hwm - 2.00.
+            self.trail_price = price 
+        elif self.percent_or_price == "percent":
+            # a percent value away from the highest water mark. 
+            # If you set this to 1.0 for a sell trailing stop, 
+            # the stop price is always hwm * 0.99.
+            self.trail_percent = percent
+
 
     def execute(self):
-        return super().execute()
+        if self.percent_or_price == "price":
+            api.submit_order(
+                symbol=self.symbol,
+                qty=self.quantity,
+                side='sell',
+                type='trailing_stop',
+                trail_price=self.trail_price,
+                time_in_force='gtc'
+            )
+            print(Fore.GREEN + "[+] new trailing stop order (trail price) submitted" + Fore.RESET + '\n')
+
+        
+        elif self.percent_or_price == "price":
+            api.submit_order(
+                symbol=self.symbol,
+                qty=self.quantity,
+                side='sell',
+                type='trailing_stop',
+                trail_price=self.trail_price,
+                time_in_force='gtc'
+            )
+            print(Fore.GREEN + "[+] new trailing stop order (trail percent) submitted" + Fore.RESET + '\n')
